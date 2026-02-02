1f163f52377e2adcd2e99a0e41d6975c81faebd3
Ayush Upadhyay <puskaru202@gmail.com>
2026-02-02 19:55:00 +0530

    add some more information

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { RestaurantProvider } from "./context/RestaurantContext.jsx";
+import { RestaurantProvider } from "./context/RestaurantProvider.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
