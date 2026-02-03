e4af7ed1c3474ff8380564b97519f3f5c689cc26
Ayush Upadhyay <puskaru202@gmail.com>
2026-02-03 11:02:57 +0530

    Fix CORS for Vercel frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173" , "https://ai-restaurant-assistant-hs0j.onrender.com"],
+    allow_origins=["http://localhost:5173" , "https://ai-restaurant-assistant-5tecgbxo8.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
